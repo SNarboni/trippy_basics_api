63c4b2914107ff1005deabdaaf0f0b5319b5669a
Seb <mac@mbp-de-seb.home>
2021-03-11 16:13:09 +0100

    ajoute du post

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const hotelModel = require("./models/hotel");
 const restaurantModel = require("./models/restaurant");
@@ -12,22 +13,34 @@ const port = 8000;
 const app = express();
 
 app.listen(port, () => {
-    console.log("server start :", port)
-})
+  console.log(`conecté a http://localhost:${port}/hotels`);
+});
+
+app.use(bodyParser.json());
 
 app.get("/hotels", async (req, res) => {
-    const hotels = await hotelModel.find()  
-    res.json(hotels);
+  const hotels = await hotelModel.find();
+  res.json(hotels);
 });
-
 
 app.get("/hotels/:id", async (req, res) => {
-    const hotels = await hotelModel.find()
-    res.json(hotels);
+  const hotels = await hotelModel.findById(req.params.id, function (err, user) {
+    res.json(user);
+  });
 });
 
-app.post("hotels", async (req, res) => {
-    await hotelModel.create({
+app.post("/hotels", async (req, res) => {
+  await hotelModel.create({
+    name: req.body.name,
+    address: req.body.address,
+    city: req.body.city,
+    country: req.body.country,
+    stars: req.body.stars,
+    hasSpa: req.body.hasSpa,
+    hasPool: req.body.hasPool,
+    priceCategory: req.body.priceCategory,
+  });
 
-    })
-})
+  res.send(`${req.body.name} à bien été ajouté à la liste`);
+  console.log(req.body);
+});
