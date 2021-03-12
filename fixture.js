e422bc211d9c7de3306d15519e3333652a35c6d0
Seb <mac@MacBook-Pro-de-Seb.local>
2021-03-12 02:01:30 +0100

    ajout de système de pagination

--- fixture.js
+++ fixture.js
@@ -7,38 +7,110 @@ mongoose.connect("mongodb://localhost:27017/trippy_binome", () => {
   console.log("connecté");
 });
 
-const createHotel = async () => {
-  await hotelModel.deleteMany({}).exec();
-  const result = await hotelModel.create([
-    {
-      name: "Hotel de seb",
-      address: "rue de la belle vie",
+//HOTELS
+
+const hotelName = [
+  "Grand Hôtel Lévèque",
+  "Hôtel Amélie",
+  "Hôtel Lindbergh",
+  "Hôtel de Lille",
+  "Hôtel Rive Gauche",
+  "Hôtel La Serre",
+  "Hôtel St-Dominique",
+  "Hôtel Le Pavillon",
+  "Hôtel Valadon",
+  "Hôtel Innova",
+  "Hôtel Lutèce",
+  "Hôtel de Venise ",
+  "Hôtel de France ",
+  "Hôtel Latour Maubourg",
+  "Hôtel Pas de Calais",
+  "Hôtel Bac Saint Germain",
+  "Hôtel Chomel",
+  "Hôtel Germain",
+  "Hôtel Bersoly’s StGermain",
+  "Hôtel de l’Académie",
+  "Hôtel Beauvoir",
+  "Hôtel Riviera Elysées",
+];
+
+const hotelAdress = [
+  "10 rue Cler",
+  "20 rue Amélie",
+  "30 rue Saint Dominique",
+  "40 rue Valadon",
+  "50 Boulevard Pasteur",
+  "60 rue de Langeac",
+  "70 rue Chaligny",
+  "80 rue Monge",
+  "90 rue de Grenelle",
+  "100 rue des Saints Pères",
+  "110 rue de Chomel",
+  "120 rue du Bac",
+  "130 rue de Lille",
+  "140 avenue Georges Bernanos",
+  "150 rue des Acacias",
+  "160 rue de Bourgogne",
+  "170 Boulevard Jules Ferry",
+  "180 rue de Vitruve",
+  "190 Boulevard Kellermann",
+  "200 rue Tiquetonne",
+  "210 rue riquet",
+  "220 rue de belleville",
+];
+
+const myHotels = [];
+
+function createMyHotels() {
+  for (let i = 0; i <= 21; i++) {
+    myHotels.push({
+      name: hotelName[i],
+      address: hotelAdress[i],
       city: "Paris",
       country: "France",
-      stars: 5,
-      hasSpa: true,
-      hasPool: true,
-      priceCategory: 3,
-    },
-  ]);
+      stars: Math.floor(Math.random() * 6),
+      hasSpa: Math.random() > 0.5 ? true : false,
+      hasPool: Math.random() > 0.5 ? true : false,
+      priceCategory: Math.floor(Math.random() * 4),
+    });
+  }
+}
+
+const createHotel = async () => {
+  createMyHotels();
+  await hotelModel.deleteMany({}).exec();
+  const result = await hotelModel.create(myHotels);
   console.log(result);
 };
 
-const createRestaurant = async () => {
-  await restaurantModel.deleteMany({}).exec();
-  const result = await restaurantModel.create([
-    {
-      name: "Radison",
+//HOTELS
+
+//RESTAURANTS
+
+const myRestaurants = [];
+
+function createMyRestaurants() {
+  for (let i = 0; i <= 21; i++) {
+    myRestaurants.push({
+      name: `Radison${i}`,
       address: "10 avenue de france",
       city: "Paris",
       country: "France",
       stars: 3,
       cuisine: true,
       priceCategory: 3,
-    },
-  ]);
+    });
+  }
+}
+
+const createRestaurant = async () => {
+  createMyRestaurants();
+  await restaurantModel.deleteMany({}).exec();
+  const result = await restaurantModel.create(myRestaurants);
   console.log(result);
 };
+
+//RESTAURANTS
 
 createHotel();
 createRestaurant();
--- index.js
+++ index.js
@@ -5,37 +5,130 @@ const bodyParser = require("body-parser");
 const hotelModel = require("./models/hotel");
 const restaurantModel = require("./models/restaurant");
 
+const port = 8000;
+const app = express();
+
+app.use(bodyParser.json());
+
 mongoose.connect("mongodb://localhost:27017/trippy_binome", () => {
   console.log("connecté");
 });
-
-const port = 8000;
-const app = express();
 
 app.listen(port, () => {
   console.log(`conecté a http://localhost:${port}/hotels`);
 });
 
-app.use(bodyParser.json());
+// HOTELS
 
 app.get("/hotels", async (req, res) => {
-  const hotels = await hotelModel.find();
+  let limitpage = parseInt(req.query.limit);
+  let offset = parseInt(req.query.page) * limitpage;
+  const hotels = await hotelModel.find().limit(limitpage).skip(offset);
   res.json(hotels);
+  console.log(hotels);
+  console.log("------------------");
+  console.log("------------------");
+  console.log("------------------");
 });
 
 app.get("/hotels/:id", async (req, res) => {
-  const hotels = await hotelModel.findById(req.params.id, function (err, user) {
-    res.json(user);
-  });
+  const hotels = await hotelModel.findById(
+    req.params.id,
+    function (err, hotel) {
+      res.json(hotel);
+    }
+  );
 });
 
 app.post("/hotels", async (req, res) => {
-  await hotelModel.create(req.body);
-
-  res.send(`${req.body.name} à bien été ajouté à la liste`);
-  console.log(req.body);
+  try {
+    await hotelModel.create(req.body);
+    res.send(`${req.body.name} à bien été ajouté à la liste`);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.put("/hotels/:id?name=newName", async (req, res) => {
-  await hotelModel.updateOne({});
+app.put("/hotels/:id", async (req, res) => {
+  hotelModel
+    .findById(req.params.id)
+    .then((model) => {
+      return Object.assign(model, req.query);
+    })
+    .then((model) => {
+      return model.save();
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+  res.send("modifié");
+});
+
+app.delete("/hotels/:id", async (req, res) => {
+  try {
+    await hotelModel.deleteOne({ _id: req.params.id });
+    res.send(`hotel supprimé`);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// HOTELS
+
+// RESTAURANT
+
+app.get("/restaurants", async (req, res) => {
+  let limitpage = parseInt(req.query.limit);
+  let offset = parseInt(req.query.page) * limitpage;
+  const restaurants = await restaurantModel
+    .find()
+    .limit(limitpage)
+    .skip(offset);
+  res.json(restaurants);
+  console.log(restaurants);
+  console.log("------------------");
+  console.log("------------------");
+  console.log("------------------");
+});
+
+app.get("/restaurants/:id", async (req, res) => {
+  const restaurants = await restaurantModel.findById(
+    req.params.id,
+    function (err, resto) {
+      res.json(resto);
+    }
+  );
+});
+
+app.post("/restaurants", async (req, res) => {
+  try {
+    await restaurantModel.create(req.body);
+    res.send(`${req.body.name} à bien été ajouté à la liste`);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  restaurantModel
+    .findById(req.params.id)
+    .then((model) => {
+      return Object.assign(model, req.query);
+    })
+    .then((model) => {
+      return model.save();
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+  res.send("modifié");
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  try {
+    await restaurantModel.deleteOne({ _id: req.params.id });
+    res.send(`restaurant supprimé`);
+  } catch (err) {
+    console.log(err);
+  }
 });
